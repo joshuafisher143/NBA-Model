dfe6c7dd9bd3ba5f58090a465ea601da83f4bad4
Joshua Fisher <joshuafisher143@yahoo.com>
2021-10-27 15:26:43 -0400

    misspelled environ

--- model/main.py
+++ model/main.py
@@ -89,7 +89,7 @@ def get_EV(bet1, bank_roll, daily_file, prob_win_dict):
     #   "client_x509_cert_url": os.environ['CLIENT_x509_CERT_URL']
     # }
     # #save feed api to google sheet
-    gs_credentials = os.envrion.get('GOOGLE_APPLICATION_CREDENTIALS')
+    gs_credentials = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
     gs_credentials_data = json.loads(gs_credentials)
     
     pd_to_gs(live_df, 'Datafeeds', gs_credentials_data)
